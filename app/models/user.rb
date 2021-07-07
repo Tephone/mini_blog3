c07782cc42ab4bf407931575c75f9f26c96b0e5d
naoko kasahara <kassa0918@gmail.com>
2021-07-08 01:01:59 +0900

    userのshow及びedit, validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :profile, length: {maximum: 200} 
+  validates :name, presence: true,
+                    length: {maximum: 20},
+                    format:{with: /\A[a-zA-Z]+\z/}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
+  resources :users, only: %i[index show]
   resources :blogs
   root 'blogs#index'
 end
