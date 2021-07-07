b5cd219d7f8e7015d421fa9199bb21a7b0c64ff8
naoko kasahara <kassa0918@gmail.com>
2021-07-08 04:34:30 +0900

    production環境basc認証、SSL/TLS通信固定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
+  protected
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name profile blog_url])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name profile blog_url])
