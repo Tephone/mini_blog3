372bda13c6885d7fca1d0e9e7879ec193d6d576b
naoko kasahara <kassa0918@gmail.com>
2021-07-08 04:14:59 +0900

    フォロー機能,validation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
   resources :users, only: %i[index show]
   resources :blogs
+  resources :relationships, only: %i[create destroy]
   root 'blogs#index'
 end
-
