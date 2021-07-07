924dfd12cd58943146d4cd36ed4a07f016ce170c
naoko kasahara <kassa0918@gmail.com>
2021-07-08 02:14:02 +0900

    　userとrelationshipsアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
                     length: {maximum: 20},
                     format:{with: /\A[a-zA-Z]+\z/}
   has_many :blogs, dependent: :destroy
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 end
